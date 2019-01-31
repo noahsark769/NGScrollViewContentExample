0389cd015c8d3c8d12bf48aff10237bb1115a8a2
Noah Gilmore <noah.w.gilmore@gmail.com>
2019-01-31 09:49:01 -0800

    It actually works

--- NGScrollViewContentExample/ViewController.swift
+++ NGScrollViewContentExample/ViewController.swift
@@ -8,13 +8,126 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+extension UIColor {
+    static func random() -> UIColor {
+        return UIColor(hue: CGFloat(drand48()), saturation: 1, brightness: 1, alpha: 1)
+    }
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+class ViewController: UIViewController {
+    private let verticalStackView: UIStackView = {
+        let view = UIStackView()
+        view.axis = .vertical
+        view.spacing = 20
+        return view
+    }()
+    private let scrollView = UIScrollView()
+    private let contentView = UIView()
+    private var hasSetInitialZoomScale = false
+
+    override func loadView() {
+        view = scrollView
+        scrollView.backgroundColor = .white
+        contentView.backgroundColor = .white
+        contentView.addSubview(verticalStackView)
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        verticalStackView.translatesAutoresizingMaskIntoConstraints = false
+
+        contentView.leadingAnchor.constraint(equalTo: verticalStackView.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: verticalStackView.trailingAnchor).isActive = true
+        contentView.topAnchor.constraint(equalTo: verticalStackView.topAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: verticalStackView.bottomAnchor).isActive = true
+
+        scrollView.addSubview(contentView)
+
+//        contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+//        contentView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+
+        scrollView.maximumZoomScale = 20
+        scrollView.minimumZoomScale = 0.1
+        scrollView.delegate = self
+
+        self.addColumn()
+        self.addColumn()
+        self.addColumn()
+        self.addColumn()
+        self.addColumn()
+        self.addColumn()
+        self.addColumn()
+        self.addColumn()
+        self.addColumn()
+        self.addColumn()
+        self.addColumn()
+        self.addColumn()
+        self.addColumn()
+        self.addRow()
+        self.addRow()
+        self.addRow()
+        self.addRow()
+        self.addRow()
+        self.addRow()
+        self.addRow()
+        self.addRow()
+        self.addRow()
+        self.addRow()
+        self.addRow()
+        self.addRow()
     }
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
 
+//        contentView.layoutIfNeeded()
+
+        let widthScale = view.bounds.size.width / contentView.bounds.width
+        let heightScale = view.bounds.size.height / contentView.bounds.height
+        let minScale = min(widthScale, heightScale)
+        scrollView.minimumZoomScale = minScale
+
+        if !hasSetInitialZoomScale {
+            scrollView.zoomScale = minScale
+            hasSetInitialZoomScale = true
+        }
+    }
+
+    private func addColumn() {
+        if verticalStackView.arrangedSubviews.isEmpty {
+            let newStackView = UIStackView()
+            newStackView.axis = .horizontal
+            newStackView.spacing = 20
+            verticalStackView.addArrangedSubview(newStackView)
+        }
+        for stackView in verticalStackView.arrangedSubviews {
+            guard let stackView = stackView as? UIStackView else { continue }
+            stackView.addArrangedSubview(self.newColorView())
+        }
+//        verticalStackView.addArrangedSubview(newColorView())
+    }
+
+    private func addRow() {
+        let numViewsToAdd = (verticalStackView.arrangedSubviews[0] as? UIStackView)?.arrangedSubviews.count ?? 0
+        let newStackView = UIStackView()
+        newStackView.axis = .horizontal
+        newStackView.spacing = 20
+        for _ in 0..<numViewsToAdd {
+            newStackView.addArrangedSubview(self.newColorView())
+        }
+        verticalStackView.addArrangedSubview(newStackView)
+    }
+
+    private func newColorView() -> UIView {
+        let colorView = UIView()
+        colorView.backgroundColor = UIColor.random()
+        colorView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        colorView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+//        colorView.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        return colorView
+    }
+}
+
+extension ViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.contentView
+    }
 }
 
